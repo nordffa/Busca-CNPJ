2be0a0bd7577d9f7317218754b3ac69709f9d59b
Gabriel Vieira <nordffa@gmail.com>
2024-05-09 00:08:38 -0300

    09/05/2024

--- main.py
+++ main.py
@@ -19,34 +19,36 @@ def busca_cep(numero_cep: str):
 
 
 while True:
-    cnpj = str(input("Digite o CNPJ a ser consultado: ")).strip()
+    cnpj = str(input("DIGITE UM CNPJ A SER CONSULTADO: ")).strip()
     numero_cnpj = extrair_numeros(cnpj)
     try:
         dados_cnpj = busca_cnpj(numero_cnpj)
     except:
         system("cls")
-        print(colored("CNPJ inválido, tente novamente.", "red"))
+        print(colored("CNPJ INVÁLIDO, TENTE NOVAMENTE.", "red"))
     else:
         try:
             dados_cep = busca_cep(extrair_numeros(dados_cnpj["cep"]))
         except:
-            print(colored("CEP inválido, tente novamente.", "red"))
+            system("cls")
+            print(colored("OCORREU UM ERRO, TENTE NOVAMENTE.", "red"))
         else:
             if dados_cnpj["status"] != "OK":
                 system("cls")
-                print(colored("CNPJ inválido, tente novamente.", "red"))
+                print(colored("CNPJ INVÁLIDO, TENTE NOVAMENTE.", "red"))
             else:
                 system("cls")
-                print(f'RAZAO SOCIAL: {dados_cnpj["nome"].upper()}')
+                print(colored("BUSCA REALIZADA COM SUCESSO!", "green"))
+                print(f'\nRAZÃO SOCIAL: {dados_cnpj["nome"].upper()}')
                 if 'fantasia' in dados_cnpj and dados_cnpj["fantasia"] != "":
                     print(f'NOME FANTASIA: {dados_cnpj["fantasia"].upper()}')
                 print(f'LOGRADOURO: {dados_cep["logradouro"].upper()}')
-                print(f'NUMERO: {int(dados_cnpj["numero"])}')
+                print(f'NÚMERO: {int(dados_cnpj["numero"])}')
                 if 'complemento' in dados_cnpj and dados_cnpj['complemento'] != "":
                     print(f'COMPLEMENTO: {dados_cnpj["complemento"].upper()}')
                 print(f'BAIRRO: {dados_cep["bairro"].upper()}')
                 print(f'LOCALIDADE: {dados_cep["localidade"].upper()}')
                 print(f'UF: {dados_cep['uf'].upper()}')
-                print(f'CEP: {dados_cep["cep"].upper()}')
+                print(f'CEP: {dados_cep["cep"].upper()}\n')
                 break
 system("PAUSE")
